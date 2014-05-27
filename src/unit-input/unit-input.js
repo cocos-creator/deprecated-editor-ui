5617c9023a9d7eb0e3ead1d7f12594574562d96b
Johnny Wu <KarasAya@gmail.com>
2014-05-27 14:50:16 +0800

    update

--- src/unit-input/unit-input.js
+++ src/unit-input/unit-input.js
@@ -1,4 +1,11 @@
 (function () {
     Polymer('fire-ui-unit-input', {
+        // var input = element.querySelector('.input');
+        // input.addEventListener( 'focusin', function () {
+        //     element.classList.add('focused');
+        // } );
+        // input.addEventListener( 'focusout', function () {
+        //     element.classList.remove('focused');
+        // } );
     });
 })();
