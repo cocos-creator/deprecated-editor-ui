3c6784816af0f2aa499f77ca048a88f720742f7a
Wu Jie <KarasAya@gmail.com>
2014-06-04 00:27:51 +0800

    add convert code in unitinput

--- src/unitinput/unitinput.js
+++ src/unitinput/unitinput.js
@@ -2,6 +2,26 @@
     Polymer('fire-ui-unitinput', {
         ready: function() {
             this.focused = false;
+            this.type = this.type ? this.type : 'int';
+            this.unit = this.unit ? this.unit : '';
+        },
+
+        _convert: function ( val ) {
+            switch ( this.type ) {
+                case 'int': 
+                    val = parseInt(val);
+                if ( isNaN(val) ) 
+                    val = 0;
+                return val;
+
+                case 'float': 
+                    val = parseFloat(val);
+                if ( isNaN(val) ) 
+                    val = 0;
+                return val;
+            }
+            console.log("can't find proper type for " + this.type);
+            return val;
         },
 
         onFocusIn: function () {
@@ -10,38 +30,41 @@
         },
 
         onFocusOut: function () {
+            if ( this.value !== this.lastVal ) {
+                this.value = this._convert(this.value);
+            }
             this.focused = false;
         },
 
-        onInput: function () {
-            this.value = this.$.input.value;
+        onInput: function (event) {
+            this.value = this._convert(event.target.value);
         },
 
-        onInputClick: function () {
-            this.$.input.select();
+        onInputClick: function (event) {
+            event.target.select();
         },
 
-        onInputKeyDown: function () {
+        onInputKeyDown: function (event) {
             switch ( event.which ) {
                 // enter
                 case 13:
-                    this.$.input.blur(); 
+                    event.target.blur(); 
                 break;
 
                 // esc
                 case 27:
-                    this.value = this.lastVal;
-                    this.$.input.blur(); 
+                    this.value = this._convert(this.lastVal);
+                    event.target.blur(); 
                 break;
             }
         },
 
         onIncrease: function () {
-            this.value += 1;
+            this.value = this._convert(this.value+1);
         },
 
         onDecrease: function () {
-            this.value -= 1;
+            this.value = this._convert(this.value-1);
         },
     });
 })();
