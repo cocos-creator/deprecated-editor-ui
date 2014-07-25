07c3ebe1cff31db593c4b440a4550cc6da1550d1
Johnny Wu <KarasAya@gmail.com>
2014-07-25 12:31:55 +0800

    allow select in list item

--- src/list/item/item.js
+++ src/list/item/item.js
@@ -1,8 +1,37 @@
 (function () {
-    Polymer('fire-ui-item', {
+    Polymer('fire-ui-li', {
+        created: function () {
+            this.selected = false;
+        },
+
         delClickAction: function ( event ) {
             this.fire("delete");
             event.stopPropagation();
+        },
+
+        toggle: function () {
+            if ( this.selected ) {
+                this.unselect();
+            }
+            else {
+                this.select();
+            }
+        },
+
+        select: function () {
+            if ( this.selected )
+                return;
+
+            this.selected = true;
+            this.classList.toggle('selected', this.selected);
+        },
+
+        unselect: function () {
+            if ( this.selected === false )
+                return;
+
+            this.selected = false;
+            this.classList.toggle('selected', this.selected);
         },
     });
 })();
--- src/list/list.js
+++ src/list/list.js
@@ -2,6 +2,7 @@
     Polymer('fire-ui-list', {
         created: function () {
             this.focused = false;
+            this.selection = [];
         },
 
         ready: function () {
@@ -20,8 +21,47 @@
             if ( FIRE.find( this.shadowRoot, event.relatedTarget ) )
                 return;
 
+            this.clearSelect();
             this.focused = false;
             this.classList.toggle('focused', this.focused);
+        },
+
+        deleteAction: function (event, detail, sender) {
+            console.log("deleteAction");
+            event.stopPropagation();
+        },
+
+        clickAction: function (event) {
+            if ( event.target instanceof FireListItem ) {
+                this.clearSelect();
+                this.select( [event.target] );
+            }
+            event.stopPropagation();
+        },
+
+        select: function ( items ) {
+            for ( var i = 0; i < items.length; ++i ) {
+                var item = items[i];
+                if ( item.selected === false ) {
+                    this.selection.push(item);
+                    item.select();
+                }
+            }
+        },
+
+        unselect: function ( items ) {
+            for ( var i = 0; i < items.length; ++i ) {
+                var item = items[i];
+                if ( item.selected ) {
+                    item.unselect();
+                    this.selection.splice(i,1);
+                }
+            }
+        },
+
+        clearSelect: function () {
+            this.unselect(this.selection);
+            this.selection = [];
         },
     });
 })();
