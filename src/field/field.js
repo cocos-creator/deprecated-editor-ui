4ad315444190b797d8b4b8ad3bba13f56dd953ab
Johnny Wu <KarasAya@gmail.com>
2014-07-28 15:22:09 +0800

    add name-field

--- src/field/field.js
+++ src/field/field.js
@@ -1,7 +1,6 @@
 (function () {
     Polymer('fire-ui-field', {
         publish: {
-            name: '',
             value: null,
             type: '',
             enumType: null,
@@ -9,17 +8,22 @@
             textMode: 'single',
         },
 
-        created: function () {
-            this.focused = false;
-        },
-
         attached: function () {
-            if ( this.name === '' ) {
-                var varName = this.attributes.value.value;
-                varName = varName.replace( /{{(.*)}}/, "$1" );
-                this.name = FIRE.camelCaseToHuman(varName); 
+            var fieldEL = this.createFieldElement();
+
+            if ( fieldEL === null ) {
+                console.error("Failed to create field" );
+                return;
             }
 
+            fieldEL.classList.add('flex-2');
+            fieldEL.bind( 'value', new PathObserver(this,'value') );
+            fieldEL.id = "field";
+            this.shadowRoot.appendChild(fieldEL);
+            this.$.field = fieldEL;
+        },
+
+        createFieldElement: function () {
             // do dom transform
             var typename = typeof this.value;
             var fieldEL = null;
@@ -100,54 +104,7 @@
                     break;
             }
 
-            if ( fieldEL === null ) {
-                console.error("Failed to create field " + this.name );
-                return;
-            }
-
-            fieldEL.classList.add('flex-2');
-            fieldEL.bind( 'value', new PathObserver(this,'value') );
-            fieldEL.id = "field";
-            // this.shadowRoot.appendChild(fieldEL);
-            this.$.focus.appendChild(fieldEL);
-            this.$.field = fieldEL;
+            return fieldEL;
         },
-
-        focusinAction: function ( event ) {
-            this.focused = true;
-            this.classList.toggle('focused', this.focused);
-            this.$.label.classList.toggle('focused', this.focused);
-            this.$.field.classList.toggle('focused', this.focused);
-        },
-
-        focusoutAction: function ( event ) {
-            if ( this.focused === false )
-                return;
-
-            if ( FIRE.find( this.shadowRoot, event.relatedTarget ) )
-                return;
-
-            this.focused = false;
-            this.classList.toggle('focused', this.focused);
-            this.$.label.classList.toggle('focused', this.focused);
-            this.$.field.classList.toggle('focused', this.focused);
-        },
-
-        mousedownAction: function ( event ) {
-            if ( this.$.focus !== event.target &&
-                 this.$.label !== event.target && 
-                 FIRE.find(this.$.label, event.target) === false )
-                return;
-
-            var focusableEL = FIRE.getFirstFocusableChild(this.$.field.shadowRoot);
-            if ( focusableEL ) {
-                focusableEL.focus();
-            }
-
-            event.preventDefault();
-            event.stopPropagation();
-
-            return;
-        }
     });
 })();
---
+++ src/name-field/name-field.js
@@ -0,0 +1,68 @@
+(function () {
+    Polymer('fire-ui-name-field', {
+        publish: {
+            name: '',
+        },
+
+        created: function () {
+            this.focused = false;
+        },
+
+        attached: function () {
+            if ( this.name === '' ) {
+                var varName = this.attributes.value.value;
+                varName = varName.replace( /{{(.*)}}/, "$1" );
+                this.name = FIRE.camelCaseToHuman(varName); 
+            }
+
+            var fieldEL = this.createFieldElement();
+            if ( fieldEL === null ) {
+                console.error("Failed to create field " + this.name );
+                return;
+            }
+
+            fieldEL.classList.add('flex-2');
+            fieldEL.bind( 'value', new PathObserver(this,'value') );
+            fieldEL.id = "field";
+            this.$.focus.appendChild(fieldEL);
+            this.$.field = fieldEL;
+        },
+
+        focusinAction: function ( event ) {
+            this.focused = true;
+            this.classList.toggle('focused', this.focused);
+            this.$.label.classList.toggle('focused', this.focused);
+            this.$.field.classList.toggle('focused', this.focused);
+        },
+
+        focusoutAction: function ( event ) {
+            if ( this.focused === false )
+                return;
+
+            if ( FIRE.find( this.shadowRoot, event.relatedTarget ) )
+                return;
+
+            this.focused = false;
+            this.classList.toggle('focused', this.focused);
+            this.$.label.classList.toggle('focused', this.focused);
+            this.$.field.classList.toggle('focused', this.focused);
+        },
+
+        mousedownAction: function ( event ) {
+            if ( this.$.focus !== event.target &&
+                 this.$.label !== event.target && 
+                 FIRE.find(this.$.label, event.target) === false )
+                return;
+
+            var focusableEL = FIRE.getFirstFocusableChild(this.$.field.shadowRoot);
+            if ( focusableEL ) {
+                focusableEL.focus();
+            }
+
+            event.preventDefault();
+            event.stopPropagation();
+
+            return;
+        }
+    });
+})();
