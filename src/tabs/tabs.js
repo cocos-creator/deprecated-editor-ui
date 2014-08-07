7f4b1c924e44c65551bc362614de955e4e7a75b9
Johnny Wu <KarasAya@gmail.com>
2014-08-07 19:17:39 +0800

    update tab and tabs style

--- src/tabs/tabs.js
+++ src/tabs/tabs.js
@@ -1,4 +1,32 @@
 (function () {
     Polymer('fire-ui-tabs', {
+        created: function () {
+            this.currentTab = null;
+        },
+
+        ready: function () {
+            for ( var i = 0; i < this.children.length; ++i ) {
+                var el = this.children[i]; 
+                if ( el instanceof FireTab ) {
+                    if ( this.currentTab === null )
+                        this.currentTab = el;
+                    el.classList.remove('active');
+                }
+            }
+
+            if ( this.currentTab !== null ) {
+                this.currentTab.classList.add('active');
+            }
+        },
+
+        clickAction: function ( event ) {
+            if ( event.target instanceof FireTab ) {
+                if ( this.currentTab !== null ) {
+                    this.currentTab.classList.remove('active');
+                }
+                this.currentTab = event.target;
+                this.currentTab.classList.add('active');
+            }
+        },
     });
 })();
