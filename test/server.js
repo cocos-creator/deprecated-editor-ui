365b5b37e3a85735ba002ec578a08a86289c032c
Johnny Wu <KarasAya@gmail.com>
2014-09-04 12:12:15 +0800

    adjust test server codes

--- test/server.js
+++ test/server.js
@@ -1,10 +1,22 @@
 // init express
 var express = require('express');
 var app = express();
+var cwd = process.cwd();
+var port = 8082;
 
-// create an error with .status. we
-// can then use the property in our
-// custom error handler (Connect repects this prop as well)
+process.on('uncaughtException', function(err) {
+    switch (err.code) {
+    case 'EADDRINUSE':
+        console.error( 'Error: The listening port ' + port + ' is in used' );
+        break;
+
+    default:
+        console.error(err);
+        break;
+    }
+});
+
+// create an error with .status.
 function error(status, msg) {
     var err = new Error(msg);
     err.status = status;
@@ -13,40 +25,32 @@ function error(status, msg) {
 
 // routes
 app.get('/', function(req, res){
-    res.sendfile('test/index.html');
+    res.sendFile( cwd + '/test/index.html' );
 });
 app.get('/dock', function(req, res){
-    res.sendfile('test/dock.html');
+    res.sendFile( cwd + '/test/dock.html');
 });
 app.get('/debug-focus', function(req, res){
-    res.sendfile('test/debug-focus.html');
+    res.sendFile( cwd + '/test/debug-focus.html');
 });
 
 // serves all the static files
 app.get(/^(.+)$/, function(req, res){ 
     // console.log('static file request : ' + req.params);
-    res.sendfile( __dirname + req.params[0]); 
+    res.sendFile( cwd + req.params[0]); 
 });
 
-// middleware with an arity of 4 are considered
-// error handling middleware. When you next(err)
-// it will be passed through the defined middleware
-// in order, but ONLY those with an arity of 4, ignoring
-// regular middleware.
 app.use(function(err, req, res, next){
     // whatever you want here, feel free to populate
     // properties on `err` to treat it differently in here.
-    res.send(err.status || 500, { error: err.message });
+    res.status(err.status || 500).send({ error: err.message });
 });
 
-// our custom JSON 404 middleware. Since it's placed last
-// it will be the last middleware called, if all others
-// invoke next() and do not respond.
 app.use(function(req, res){
-    res.send(404, { error: "404 Error." });
+    res.status(404).send({ error: "404 Error." });
 });
 
 // start the server
-var server = app.listen(8082, function() {
+var server = app.listen(port, function() {
     console.log('Listening on port %d', server.address().port);
 });
