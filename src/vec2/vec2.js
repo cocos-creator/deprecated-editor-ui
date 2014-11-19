ec7dc60590b87e86259b8f6200048c303e6a0fff
w88975 <1457434343@qq.com>
2014-11-19 10:43:59 +0800

    增加disabled属性，但未实现功能

--- src/vec2/vec2.js
+++ src/vec2/vec2.js
@@ -2,7 +2,13 @@
     Polymer({
         publish: {
             value: null,
+
+            disabled: {
+                value: false,
+                reflect: true
+            },
         },
+
 
         created: function () {
             this.value = new Fire.Vec2(0,0);
@@ -11,7 +17,6 @@
         changedAction: function ( event ) {
             this.value = new Fire.Vec2(this.$.x.value, this.$.y.value);
             event.stopPropagation();
-
             this.fire("changed");
         },
     });
