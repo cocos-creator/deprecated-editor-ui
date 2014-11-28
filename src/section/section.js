5ceb9e29a3cdae226bb370e23f826e9a027fecb2
Johnny Wu <KarasAya@gmail.com>
2014-11-28 15:19:59 +0800

    let section use focusable

--- src/section/section.js
+++ src/section/section.js
@@ -1,52 +1,23 @@
 (function () {
-    Polymer({
+    Polymer(EditorUI.mixin({
         publish: {
             name: '',
-            focused: {
+            folded: {
                 value: false,
                 reflect: true
             },
         },
 
         created: function () {
-            this.folded = false;
-
-            // TODO:
-            // this.addEventListener( "keydown", function ( e ) {
-            //     if ( e.keyCode === 9 ) {
-            //         if ( e.shiftKey ) {
-            //             console.log("up");
-            //         }
-            //         else {
-            //             console.log("down");
-            //         }
-            //         e.preventDefault();
-            //         e.stopPropagation();
-            //     }
-            // }, true );
         },
 
         ready: function() {
-            this.$.title.tabIndex = EditorUI.getParentTabIndex(this)+1;
+            this._init(this.$.title);
         },
 
         clickAction: function (event) {
             this.folded = !this.folded;
             event.stopPropagation();
         },
-
-        focusAction: function (event) {
-            this.focused = true;
-        },
-
-        blurAction: function (event) {
-            if ( this.focused === false )
-                return;
-
-            if ( EditorUI.find( this.shadowRoot, event.relatedTarget ) )
-                return;
-
-            this.focused = false;
-        },
-    });
+    }, EditorUI.focusable));
 })();
