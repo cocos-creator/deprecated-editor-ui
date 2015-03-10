33460b7edcac7703b54b1bb445b4875bf26b24ed
Johnny Wu <KarasAya@gmail.com>
2015-03-10 12:56:37 +0800

    add key up, down for list-view

--- src/list/list.js
+++ src/list/list.js
@@ -23,7 +23,28 @@ Polymer(EditorUI.mixin({
         if ( item ) {
             item.selected = true;
             this.fire('selected', item);
+            this._curSelection = item;
         }
+    },
+
+    nextItem: function ( curItem ) {
+        var idx = this.value.indexOf(curItem);
+        if ( idx !== -1 )
+            idx += 1;
+        if ( idx >= this.value.length )
+            idx = this.value.length-1;
+
+        return this.value[idx];
+    },
+
+    prevItem: function ( curItem ) {
+        var idx = this.value.indexOf(curItem);
+        if ( idx !== -1 )
+            idx -= 1;
+        if ( idx < 0 )
+            idx = 0;
+
+        return this.value[idx];
     },
 
     selectAction: function ( event ) {
@@ -34,6 +55,60 @@ Polymer(EditorUI.mixin({
             return;
 
         this.select(event.target.value);
+    },
+
+    keydownAction: function (event) {
+        switch ( event.which ) {
+            // up-arrow
+            case 38:
+                if ( this._curSelection ) {
+                    var prev = this.prevItem(this._curSelection);
+                    if ( prev ) {
+                        if ( prev !== this._curSelection ) {
+                            this.select(prev);
+
+                            window.requestAnimationFrame( function() {
+                                // HACK, +1 because there have hidden element, will fix in polymer-0.8 by using item.dom
+                                var idx = this.value.indexOf(prev)+1;
+                                var itemEL = this.$.content.children[idx];
+                                if ( itemEL.offsetTop <= this.scrollTop ) {
+                                    this.scrollTop = itemEL.offsetTop;
+                                }
+                            }.bind(this));
+                        }
+                    }
+                }
+
+                event.preventDefault();
+                event.stopPropagation();
+                break;
+
+            // down-arrow
+            case 40:
+                if ( this._curSelection ) {
+                    var next = this.nextItem(this._curSelection);
+                    if ( next ) {
+                        if ( next !== this._curSelection ) {
+                            this.select(next);
+
+                            window.requestAnimationFrame( function() {
+                                // HACK, +1 because there have hidden element, will fix in polymer-0.8 by using item.dom
+                                var idx = this.value.indexOf(next)+1;
+                                var itemEL = this.$.content.children[idx];
+                                var itemHeight = itemEL.offsetHeight;
+                                var contentHeight = this.offsetHeight - 2; // 2 for border,
+                                if ( itemEL.offsetTop + itemHeight >= this.scrollTop + contentHeight ) {
+                                    this.scrollTop = itemEL.offsetTop + itemHeight - contentHeight;
+                                }
+                            }.bind(this));
+                        }
+                    }
+                }
+
+                event.preventDefault();
+                event.stopPropagation();
+                break;
+        }
     },
 
 }, EditorUI.focusable));
